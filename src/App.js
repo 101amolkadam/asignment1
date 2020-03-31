de8b8ed16b80c0aa73c9df748a3b29e46b281398
Test <101amolkadam@gmail.com>
2020-03-31 15:30:57 +0530

    Signed-off-by: Test <101amolkadam@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Sidebar from './containers/sideBar';
 import Main from './containers/dashboard-main';
+import Login from './containers/Login';
+
 
 function App() {
   return (
     <div>
         <Sidebar />;
         <Main />;
+        {/* <Login />; */}
+        
     </div>    
   );
 }
--- src/containers/dashboard-main.js
+++ src/containers/dashboard-main.js
@@ -4,13 +4,14 @@ const Main = (props) => {
 
     const mystyle = {
         marginLeft: "250px",
+        marginBottom: "20px",
         padding: "1px 16px",
         height: "1000px",
     };
     return(
         <div style = {mystyle}>
             <h2>Asignment 1</h2>
-            <p>Thsi is asignment 1.</p>
+            <p>This is asignment 1.</p>
         </div>
     );
 }
